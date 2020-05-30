b1afb11290577a78f33b260dfcb9c270090f054f
Sam McIlroy <sam.mcilroy@gmail.com>
2020-05-31 00:45:19 +0100

    up

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -11,8 +11,9 @@ def max_pairwise_product(numbers):
 
     return max_product
 
-
 if __name__ == '__main__':
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
     print(max_pairwise_product(input_numbers))
+
+
